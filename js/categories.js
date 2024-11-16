29b5e5523300c1ef617319bf5d9750c8f60855c3
sofiasojoo <ssojo@udesa.edu.ar>
2024-11-16 12:11:12 -0300

    categories js (no terminado)

--- js/categories.js
+++ js/categories.js
@@ -1,18 +1,17 @@
 let queryString = location.search
 let queryStringObj = new URLSearchParams(queryString);
-let id = queryStringObj.get("id")
+let mealType = queryStringObj.get("mealType")
 
 let nombreCategorias= document.querySelector(".categorias")
 
 let CategoriasIndividual={
     mealType: "",
 }
-fetch(`https://dummyjson.com/recipes/${id}`)
+fetch(`https://dummyjson.com/recipes/meal-type/${mealType}`)
 .then(function(response) {
     return response.json()
   })
   .then(function(data){
-   
     
     CategoriasIndividual.mealType = data.mealType
     nombreCategorias.innerText = CategoriasIndividual.mealType
